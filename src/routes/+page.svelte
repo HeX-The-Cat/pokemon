45aaae40ef613071a4c75fe7d997cd1ab5fbd447
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2023-08-24 05:33:20 +0300

    start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,29 @@
 <script>
     import {PBIcon} from "../lib/index.js"
-  import Fetcher from "./components/Fetcher.svelte";
-
-    
 
     let pokemonName = "garchomp";
     let searchingPokemon = getPokemon();
     
-    function getPokemon() {
+    async function getPokemon() {
         if(pokemonName != ""){
+
             var pokeData = []
-            Fetcher( pokemonName, pokeData );
-            
+
+            const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
+            const data = await res.json()
+            //console.log(data)
+            pokeData.push(data)
+
+            for (let i = 0; i < (data.abilities).length; i++) {
+                const resAbi = await fetch(data.abilities[i].ability.url)
+                const dataAbit = await resAbi.json()
+
+                //console.log(dataAbit)
+                pokeData.push(dataAbit)
+            }
+
+            console.log(pokeData)
+            return pokeData
         }
     }
 
