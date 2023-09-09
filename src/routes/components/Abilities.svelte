2b07558abefd94c4a032a78505ec9500c200bfba
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2023-09-09 20:11:59 +0300

    abilities JSON fix

--- src/routes/components/Abilities.svelte
+++ src/routes/components/Abilities.svelte
@@ -1,24 +1,44 @@
 <script>
     export let pokeData;
+
+    let effects = "1"
+    
 </script>
 
 <div>
     {#each pokeData as ability, index}
         {#if index > 0}
-            <div tabindex="-1" class="collapse collapse-arrow">
+            {#if ability.effect_entries.hasOwnProperty(effects)}
+                <div tabindex="-1" class="collapse collapse-arrow">
 
-                <div class="collapse-title text-xl font-medium">
-                    <p>{ability.name.replace(/-/g, ' ').toUpperCase()}</p>
-                    {#if pokeData[0].abilities[index - 1].is_hidden == true}
-                        <div class="badge badge-outline">Hidden Ability</div>
-                    {/if}
-                </div>
+                    <div class="collapse-title text-xl font-medium">
+                        <p>{ability.name.replace(/-/g, ' ').toUpperCase()}</p>
+                        {#if pokeData[0].abilities[index - 1].is_hidden == true}
+                            <div class="badge badge-outline">Hidden Ability</div>
+                        {/if}
+                    </div>
 
-                <div class="collapse-content"> 
-                    <p>{ability.effect_entries[1].short_effect}</p>
+                    <div class="collapse-content"> 
+                        <p>{ability.effect_entries[1].short_effect}</p>
+                    </div>
+                    
                 </div>
-                
-            </div>
+            {:else}
+                <div tabindex="-1" class="collapse collapse-arrow">
+
+                    <div class="collapse-title text-xl font-medium">
+                        <p>{ability.name.replace(/-/g, ' ').toUpperCase()}</p>
+                        {#if pokeData[0].abilities[index - 1].is_hidden == true}
+                            <div class="badge badge-outline">Hidden Ability</div>
+                        {/if}
+                    </div>
+
+                    <div class="collapse-content"> 
+                        <p>{ability.flavor_text_entries[7].flavor_text}</p>
+                    </div>
+                    
+                </div>
+            {/if}
         {/if}
     {/each}
 </div>
