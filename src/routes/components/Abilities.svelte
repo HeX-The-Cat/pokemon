27abd17e22d62bec7e685bd31a4211c15a5052c5
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2023-10-12 00:21:27 +0300

    smol change

--- src/routes/components/Abilities.svelte
+++ src/routes/components/Abilities.svelte
@@ -8,7 +8,7 @@
         {#if index > 0}
             {#if ability.effect_entries.hasOwnProperty("1")}
                 <div tabindex="-1" class="collapse collapse-arrow">
-
+                    <input type="checkbox" /> 
                     <div class="collapse-title text-xl font-medium">
                         <p>{ability.name.replace(/-/g, ' ').toUpperCase()}</p>
                         {#if pokeData[0].abilities[index - 1].is_hidden == true}
