a1cf4da5a23a97fb9f1877d518a713541ea1b939
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2023-10-16 01:02:17 +0300

    no more german (hopefully)

--- src/routes/components/Abilities.svelte
+++ src/routes/components/Abilities.svelte
@@ -9,6 +9,7 @@
             {#if ability.effect_entries.hasOwnProperty("1")}
                 <div tabindex="-1" class="collapse collapse-arrow">
                     <input type="checkbox" /> 
+                    
                     <div class="collapse-title text-xl font-medium">
                         <p>{ability.name.replace(/-/g, ' ').toUpperCase()}</p>
                         {#if pokeData[0].abilities[index - 1].is_hidden == true}
@@ -16,14 +17,20 @@
                         {/if}
                     </div>
 
-                    <div class="collapse-content"> 
-                        <p>{ability.effect_entries[1].short_effect}</p>
-                    </div>
-                    
+                    {#if ability.effect_entries[0].language.name == "en"}
+                        <div class="collapse-content"> 
+                            <p>{ability.effect_entries[0].short_effect}</p>
+                        </div>
+                    {:else}
+                        <div class="collapse-content"> 
+                            <p>{ability.effect_entries[1].short_effect}</p>
+                        </div>
+                    {/if}
                 </div>
             {:else}
                 <div tabindex="-1" class="collapse collapse-arrow">
-                    <input type="checkbox" /> 
+                    <input type="checkbox" />
+
                     <div class="collapse-title text-xl font-medium">
                         <p>{ability.name.replace(/-/g, ' ').toUpperCase()}</p>
                         {#if pokeData[0].abilities[index - 1].is_hidden == true}
@@ -34,7 +41,6 @@
                     <div class="collapse-content"> 
                         <p>{ability.flavor_text_entries[7].flavor_text}</p>
                     </div>
-                    
                 </div>
             {/if}
         {/if}
