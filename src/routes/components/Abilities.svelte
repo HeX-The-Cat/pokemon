adb9800ebd28a072e623f84ad9f2bcc68787d53b
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2024-10-06 13:04:46 +0300

    fixed ability text logic

--- src/routes/components/Abilities.svelte
+++ src/routes/components/Abilities.svelte
@@ -1,63 +1,55 @@
 <script>
   export let pokeData;
   export let selectedForm;
+
+  const getAbilityDescription = (ability) => {
+    const shortEntry = ability.effect_entries.find((entry) => entry.language.name === "en");
+    if (shortEntry && shortEntry.short_effect) {
+      return shortEntry.short_effect;
+    }
+
+    const flavorEntry = ability.flavor_text_entries.find((entry) => entry.language.name === "en");
+    if (flavorEntry) {
+      return flavorEntry.flavor_text;
+    }
+
+    return "No effect entry found for the ability";
+  };
+
+  function filterUniqueAbilities(abilities) {
+    const uniqueAbilities = [];
+    const abilityNames = new Set();
+
+    abilities.forEach((ability) => {
+      if (!abilityNames.has(ability.name)) {
+        abilityNames.add(ability.name);
+        uniqueAbilities.push(ability);
+      }
+    });
+
+    return uniqueAbilities;
+  }
+
+  const uniqueAbilities = filterUniqueAbilities(pokeData[selectedForm].abilities);
 </script>
 
 <div>
   <h3 class=" pl-4">Abilities:</h3>
 
-  {#each pokeData[selectedForm].abilities as ability, index}
-    {#if ability.effect_entries.hasOwnProperty("1")}
-      <div tabindex="-1" class="collapse collapse-arrow">
-        <input type="checkbox" />
+  {#each uniqueAbilities as ability, index}
+    <div tabindex="-1" class="collapse collapse-arrow">
+      <input type="checkbox" />
 
-        <div class="collapse-title text-xl font-medium">
-          <p>{ability.name.replace(/-/g, " ").toUpperCase()}</p>
-          {#if pokeData[selectedForm].basic.abilities[index].is_hidden == true}
-            <p class="badge badge-outline py-[10px]">Hidden Ability</p>
-          {/if}
-        </div>
-
-        {#if ability.effect_entries[0].language.name == "en"}
-          <div class="collapse-content">
-            <p>{ability.effect_entries[0].short_effect}</p>
-          </div>
-        {:else}
-          <div class="collapse-content">
-            <p>{ability.effect_entries[1].short_effect}</p>
-          </div>
+      <div class="collapse-title text-xl font-medium">
+        <p>{ability.name.replace(/-/g, " ").toUpperCase()}</p>
+        {#if pokeData[selectedForm].basic.abilities[index].is_hidden}
+          <p class="badge badge-outline py-[10px]">Hidden Ability</p>
         {/if}
       </div>
-    {:else if ability.flavor_text_entries.hasOwnProperty("7")}
-      <div tabindex="-1" class="collapse collapse-arrow">
-        <input type="checkbox" />
 
-        <div class="collapse-title text-xl font-medium">
-          <p>{ability.name.replace(/-/g, " ").toUpperCase()}</p>
-          {#if pokeData[selectedForm].basic.abilities[index].is_hidden == true}
-            <p class="badge badge-outline">Hidden Ability</p>
-          {/if}
-        </div>
-
-        <div class="collapse-content">
-          <p>{ability.flavor_text_entries[7].flavor_text}</p>
-        </div>
+      <div class="collapse-content">
+        <p>{getAbilityDescription(ability)}</p>
       </div>
-    {:else}
-      <div tabindex="-1" class="collapse collapse-arrow">
-        <input type="checkbox" />
-
-        <div class="collapse-title text-xl font-medium">
-          <p>{ability.name.replace(/-/g, " ").toUpperCase()}</p>
-          {#if pokeData[selectedForm].basic.abilities[index].is_hidden == true}
-            <p class="badge badge-outline">Hidden Ability</p>
-          {/if}
-        </div>
-
-        <div class="collapse-content">
-          <p>No effect entry found for the ability.</p>
-        </div>
-      </div>
-    {/if}
+    </div>
   {/each}
 </div>
